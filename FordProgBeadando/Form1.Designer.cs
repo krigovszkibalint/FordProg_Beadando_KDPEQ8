74d601e8ca76d1ac15501bace7ce01ae16f50a90
Krigovszki Bálint <krigovszki.balint@gmail.com>
2021-12-07 00:39:03 +0100

    Beadandó

---
+++ FordProgBeadando/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace FordProgBeadando
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+       
+
+      
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.tBInput = new System.Windows.Forms.TextBox();
+            this.rTB = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnTablazatBetolt = new System.Windows.Forms.Button();
+            this.BTNTablaTorol = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ofp = new System.Windows.Forms.OpenFileDialog();
+            this.btnSaveOutput = new System.Windows.Forms.Button();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.btnSaveTable = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(16, 11);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(732, 335);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // tBInput
+            // 
+            this.tBInput.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tBInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tBInput.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBInput.ForeColor = System.Drawing.Color.Black;
+            this.tBInput.Location = new System.Drawing.Point(774, 44);
+            this.tBInput.Margin = new System.Windows.Forms.Padding(2);
+            this.tBInput.Name = "tBInput";
+            this.tBInput.Size = new System.Drawing.Size(214, 29);
+            this.tBInput.TabIndex = 1;
+            this.tBInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // rTB
+            // 
+            this.rTB.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rTB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rTB.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rTB.ForeColor = System.Drawing.Color.Black;
+            this.rTB.Location = new System.Drawing.Point(16, 384);
+            this.rTB.Margin = new System.Windows.Forms.Padding(2);
+            this.rTB.Name = "rTB";
+            this.rTB.Size = new System.Drawing.Size(972, 187);
+            this.rTB.TabIndex = 2;
+            this.rTB.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(774, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Számolás";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnTablazatBetolt
+            // 
+            this.btnTablazatBetolt.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnTablazatBetolt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTablazatBetolt.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTablazatBetolt.ForeColor = System.Drawing.Color.White;
+            this.btnTablazatBetolt.Location = new System.Drawing.Point(774, 200);
+            this.btnTablazatBetolt.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTablazatBetolt.Name = "btnTablazatBetolt";
+            this.btnTablazatBetolt.Size = new System.Drawing.Size(214, 40);
+            this.btnTablazatBetolt.TabIndex = 4;
+            this.btnTablazatBetolt.Text = "Tábla betöltése";
+            this.btnTablazatBetolt.UseVisualStyleBackColor = false;
+            this.btnTablazatBetolt.Click += new System.EventHandler(this.BtnTablazatBetolt_Click);
+            // 
+            // BTNTablaTorol
+            // 
+            this.BTNTablaTorol.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BTNTablaTorol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTNTablaTorol.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNTablaTorol.ForeColor = System.Drawing.Color.White;
+            this.BTNTablaTorol.Location = new System.Drawing.Point(774, 306);
+            this.BTNTablaTorol.Margin = new System.Windows.Forms.Padding(2);
+            this.BTNTablaTorol.Name = "BTNTablaTorol";
+            this.BTNTablaTorol.Size = new System.Drawing.Size(214, 40);
+            this.BTNTablaTorol.TabIndex = 5;
+            this.BTNTablaTorol.Text = "Betöltött tábla törlése";
+            this.BTNTablaTorol.UseVisualStyleBackColor = false;
+            this.BTNTablaTorol.Click += new System.EventHandler(this.BTNTablaTorol_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(11, 357);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kimenet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(769, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input";
+            // 
+            // ofp
+            // 
+            this.ofp.FileName = "openFileDialog1";
+            // 
+            // btnSaveOutput
+            // 
+            this.btnSaveOutput.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSaveOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveOutput.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveOutput.ForeColor = System.Drawing.Color.White;
+            this.btnSaveOutput.Location = new System.Drawing.Point(774, 148);
+            this.btnSaveOutput.Name = "btnSaveOutput";
+            this.btnSaveOutput.Size = new System.Drawing.Size(214, 40);
+            this.btnSaveOutput.TabIndex = 9;
+            this.btnSaveOutput.Text = "Kimenet Mentése";
+            this.btnSaveOutput.UseVisualStyleBackColor = false;
+            this.btnSaveOutput.Click += new System.EventHandler(this.btnSaveOutput_Click);
+            // 
+            // btnSaveTable
+            // 
+            this.btnSaveTable.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSaveTable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveTable.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveTable.ForeColor = System.Drawing.Color.White;
+            this.btnSaveTable.Location = new System.Drawing.Point(774, 254);
+            this.btnSaveTable.Name = "btnSaveTable";
+            this.btnSaveTable.Size = new System.Drawing.Size(214, 40);
+            this.btnSaveTable.TabIndex = 10;
+            this.btnSaveTable.Text = "Tábla mentése";
+            this.btnSaveTable.UseVisualStyleBackColor = false;
+            this.btnSaveTable.Click += new System.EventHandler(this.btnSaveTable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1014, 582);
+            this.Controls.Add(this.btnSaveTable);
+            this.Controls.Add(this.btnSaveOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTNTablaTorol);
+            this.Controls.Add(this.btnTablazatBetolt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rTB);
+            this.Controls.Add(this.tBInput);
+            this.Controls.Add(this.dataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+   
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox tBInput;
+        private System.Windows.Forms.RichTextBox rTB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTablazatBetolt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog ofp;
+        private System.Windows.Forms.Button btnSaveOutput;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.Button btnSaveTable;
+        private System.Windows.Forms.Button BTNTablaTorol;
+    }
+}
+
---
+++ FordProgBeadando/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FordProgBeadando
+{
+    public partial class Form1 : Form
+    {
+        string inputFile;
+        string outputFile = "";
+        string inputFileName = "input.txt";
+        string outputFileName = "export.txt";
+        string[] inputSplit;
+        string[] inputSplitRow;    
+
+        string input;
+        string state;
+        string numbers = "";       
+
+        public Form1()
+        {
+            InitializeComponent();
+            Read(inputFileName);
+            ReadTxt();   
+        }
+
+        private void ClearTable()
+        {
+            dataGridView.Columns.Clear();
+            dataGridView.Rows.Clear();
+        }
+
+        private void WriteTxt()
+        {
+            for (int i = 0; i < dataGridView.RowCount; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < dataGridView.ColumnCount; j++)
+                    {
+                        outputFile += dataGridView.Columns[j].HeaderText;
+                        if (j < dataGridView.ColumnCount - 1)
+                            outputFile += ';';
+                    }
+                    outputFile += "\n";
+                }
+                for (int j = 0; j < dataGridView.ColumnCount; j++)
+                {
+                    if (j == 0)
+                    {
+                        outputFile += dataGridView.Rows[i].HeaderCell.Value + ";";
+                    }
+                    outputFile += dataGridView[j, i].Value;
+                    if (j < dataGridView.ColumnCount - 1)
+                        outputFile += ";";
+                    else
+                        outputFile += "\n";
+                }
+            }
+        }
+
+        private void ReadTxt()
+        {
+            inputSplit = inputFile.Split('\n');
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+                inputSplitRow = inputSplit[i].Split(';');
+                if (i == 0)
+                {
+                    for (int j = 0; j < inputSplitRow.Length; j++)
+                    {
+                        dataGridView.Columns.Add(inputSplitRow[j], inputSplitRow[j]);
+                    }
+                }
+                dataGridView.Rows.Add();
+                for (int j = 0; j < inputSplitRow.Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        if (i > 0)
+                        {
+                            dataGridView.Rows[i].HeaderCell.Value = inputSplitRow[j];
+                        }
+                    }
+                    else
+                    {
+                        dataGridView[j - 1, i].Value = inputSplitRow[j];
+                    }
+                }
+
+            }
+            dataGridView.Rows.RemoveAt(0);
+        }
+
+        public void Read(string fname)
+        {
+            StreamReader sr = new StreamReader(File.OpenRead(@fname));
+            inputFile = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        public void Write()
+        {
+            StreamWriter sw = new StreamWriter(File.OpenWrite(@outputFileName));
+            sw.Write(outputFile);
+            sw.Flush();
+            sw.Close();
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            numbers = "";
+            rTB.Text = "";
+            int rowindex = -1;
+            int columnindex = -1;          
+
+            state = dataGridView.Rows[0].HeaderCell.Value.ToString() + "#";
+            input = tBInput.Text;
+            input = Regex.Replace(input, "[0-9]", "i");
+            input = Regex.Replace(input, " ", "");
+
+            if (input[input.Length - 1] != '#')
+            {
+                input = input + "#";
+                tBInput.Text = input;
+            }
+                
+            string actState = state[0].ToString();
+            
+            rTB.AppendText(input + "," + state + "," + numbers);
+            while (true)
+            {
+                columnindex = -1;
+                rowindex = -1;
+                if (state[1].ToString() == "'") { actState = state[0].ToString() + state[1].ToString(); }
+                else { actState = state[0].ToString(); }
+                for (int i = 0; i < dataGridView.Rows.Count - 1; i++)
+                {
+                    if (actState == dataGridView.Rows[i].HeaderCell.Value.ToString())
+                    {
+                        rowindex = i;
+                    }
+                }
+
+                if (rowindex == -1)
+                {
+                    rTB.Text += string.Format("\nHiba a bemeneti állapot `{0}´ karakternél", actState);
+
+                    break;
+                }
+
+                for (int i = 0; i < dataGridView.ColumnCount; i++)
+                {
+                    if (input[0].ToString() == dataGridView.Columns[i].Name.ToString())
+                    {
+                        columnindex = 0;
+
+                    }
+                }
+                if (columnindex == -1)
+                {
+                    rTB.Text += string.Format("\nHiba a bemenetben a `{0}´ karakternél", input[0]);
+
+                    break;
+                }
+
+                string command = dataGridView[input[0].ToString(), rowindex].Value.ToString();
+                switch (command)
+                {
+                    case "pop":
+                        {
+                            input = input.Substring(1);
+                            state = state.Substring(1);
+                            rTB.AppendText("\n" + input + "," + state + "," + numbers);
+
+                            break;
+                        }
+
+                    case "error":
+                        {
+                            rTB.AppendText("\n" + input + "," + state + "," + numbers);
+                            rTB.AppendText(String.Format(" Hiba a bemenetben a `{0}´ karakternél", input[0]));
+
+                            break;
+                        }
+                    default:
+                        {
+                            try
+                            {
+                                numbers += command.Split(',')[1].Substring(0, 1);
+                                if ((command.Split(',')[0])[1] == '$')
+                                {
+                                    if (state[1].ToString() == "'") { state = state.Substring(2); }
+                                    else { state = state.Substring(1); }
+                                    rTB.AppendText("\n" + input + "," + state + "," + numbers);
+                                }
+                                else
+                                {
+                                    if (state[1].ToString() == "'") { state = command.Split(',')[0].Substring(1) + state.Substring(2); }
+                                    else { state = command.Split(',')[0].Substring(1) + state.Substring(1); }
+                                    rTB.AppendText("\n" + input + "," + state + "," + numbers);
+                                }
+                            }
+                            catch
+                            {
+                                rTB.AppendText(String.Format("A {0} oszlop, {1}. sorának utasítása hibás", input[0].ToString(), rowindex));
+                            }
+                            break;
+                        }
+                }
+                if (input == state && state == "#")
+                {
+                    rTB.AppendText("\n" + input + "," + state + "," + numbers + " elfogadva");
+                    break;
+                }
+                if (command == "error")
+                    break;
+
+                if (sw.Elapsed > TimeSpan.FromMilliseconds(500))
+                    break;
+            }        
+        }
+
+        private void BtnTablazatBetolt_Click(object sender, EventArgs e)
+        {
+            ofp.Filter = "Text|*.txt*.*";
+            ofp.ShowDialog();
+            StreamReader sr = new StreamReader(File.OpenRead(ofp.FileName));
+            inputFile = sr.ReadToEnd();
+            ClearTable();
+            ReadTxt();
+        }
+
+        private void BTNTablaTorol_Click(object sender, EventArgs e)
+        {
+            ClearTable();
+        }
+
+        private void btnSaveOutput_Click(object sender, EventArgs e)
+        {
+
+            StreamWriter writer = File.AppendText(outputFileName);
+            writer.Write(rTB.Text);
+            writer.Close();
+        }
+
+        private void btnSaveTable_Click(object sender, EventArgs e)
+        {
+            sfd.Filter = "Text|*.txt*.*";
+            sfd.ShowDialog();
+            StreamWriter sw = new StreamWriter(File.OpenWrite(@sfd.FileName));
+            WriteTxt();
+            sw.Write(outputFile);
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
